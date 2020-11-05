e543eddf207d24efee9ae877084e536ebc867cf5
AntoineSimoulin <antoine.simoulin@gmail.com>
2020-11-05 09:43:27 +0100

    :bug: remove old solution version

--- TP3 - Word Embeddings/solutions/vectorizer.py
+++ TP3 - Word Embeddings/solutions/vectorizer.py
@@ -27,8 +27,6 @@ class Vectorizer(BaseEstimator, TransformerMixin):
                 tf_idf_w = np.squeeze([tf_idf_w[self.w2idx_tfidf.get(t, 0)] for t in tokens if t in w2v_model.vocab])
                 sentence_embedding = np.average(vectors, weights=tf_idf_w, axis=0)
 
-        if self.normalize:
-            sentence_embedding = sentence_embedding / np.linalg.norm(sentence_embedding, axis=1, keepdims=True)
         return sentence_embedding
 
     def _vectorize(self, tokens):
@@ -40,7 +38,10 @@ class Vectorizer(BaseEstimator, TransformerMixin):
         self.idx_tfidf2w = {idx: w for (idx, w) in enumerate(self.tfidf_vectorizer.get_feature_names())}
         return self
 
-    def transform(self, X, y=None):
+    def transform(self, X, y=None, eps=1e-12):
         X = [self.vectorize(t) for t in X['tokens']]
         X = np.array(X)
+
+        if self.normalize:
+            X = X / np.linalg.norm(X + eps, axis=1, keepdims=True)
         return X
